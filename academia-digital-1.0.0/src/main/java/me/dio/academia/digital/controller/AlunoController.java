f4ccc557c374864ccc886e8ee6f48e76726738c0
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-03-30 09:23:38 -0300

    Aluno Validação de entrada de dados no create e update

--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -7,6 +7,7 @@ import me.dio.academia.digital.service.impl.AlunoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -27,14 +28,14 @@ public class AlunoController {
     }
 
     @PostMapping
-    public Aluno create(@RequestBody AlunoForm alunoForm){
+    public Aluno create(@Valid @RequestBody AlunoForm alunoForm){
         return service.create(alunoForm);
     }
 
     @PutMapping("/{id}")
     public Aluno update(
             @PathVariable Long id,
-            @RequestBody AlunoUpdateForm aluno){
+            @Valid @RequestBody AlunoUpdateForm aluno){
         return service.update(id, aluno);
     }
 
--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/form/AlunoUpdateForm.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/form/AlunoUpdateForm.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
@@ -11,9 +15,15 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class AlunoUpdateForm {
 
+  @NotEmpty(message = "Preencha o campo corretamente.")
+  @Size(min = 3, max =50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
   private String nome;
 
+  @NotEmpty(message = "Preencha o campo corretamente.")
+  @Size(min = 3, max =50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
   private String bairro;
 
+  @NotNull(message = "Prencha o campo corretamente.")
+  @Past(message = "Data '${validatedValue}' é inválida.")
   private LocalDate dataDeNascimento;
 }
