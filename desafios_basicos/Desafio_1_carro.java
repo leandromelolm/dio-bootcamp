1000f813fc4451074c5f565708549724feba109d
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-04-05 00:07:42 -0300

    Alterado nome dos pacote

---
+++ desafios_basicos/Desafio_1_carro.java
@@ -0,0 +1,24 @@
+package desafio_basicos;
+
+import java.util.Scanner;
+
+public class Desafio_1_carro {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int custoFabrica = scan.nextInt();
+        int porcentagemDistribuidor = scan.nextInt();
+        int PercentualImpostos = scan.nextInt();
+
+        int custoConsumidor;
+
+        int Distribuidor;
+        int ValorImpostos;
+
+        // TODO: Implemente uma condição que calcule a porcentagem do distribuidor e dos impostos:
+
+        Distribuidor = (custoFabrica * porcentagemDistribuidor) / 100;
+        ValorImpostos = (custoFabrica * PercentualImpostos) / 100;
+        custoConsumidor = custoFabrica + Distribuidor + ValorImpostos;
+        System.out.println(custoConsumidor);
+    }
+}
